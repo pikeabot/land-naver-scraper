6e2c3d8d582743e1a011a67cbd031329d9cbc6d7
op8 <jrc300@gmail.com>
2015-10-18 19:45:42 -0700

    added korean names and property type

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*- 
 import os
 import datetime
 import re
 import sys
 import urllib2
 import csv
+import codecs
 
 from os import listdir
 from os.path import isfile, join
@@ -49,11 +51,44 @@ def add_page(url, writer):
 
 	for i in range(0, len(price_list)):
 		row=[]
-		row.append('강남구')
+		
 		row.append('Gaepo')
 		row.append(price_list[i])
 		row.append(area_list[i])
 		writer.writerow(row)
+
+def test():
+	url='http://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1168010300&articleOrderCode=&siteOrderCode=&cpId=&mapX=127.0723995&mapY=37.4915256&mapLevel=10&minPrc=&maxPrc=&minWrrnt=&maxWrrnt=&minLease=&maxLease=&minSpc=&maxSpc=&subDist=&mviDate=&hsehCnt=&rltrId=&mnex=&mHscpNo=&mPtpRange=&mnexOrder=&location=&ptpNo=&bssYm='
+	soup = soupify(url)
+	row = []
+	#titles = soup.find_all('td', class_='sale_type2')
+	titles = soup.find_all('tr', class_='evennum')
+	t= titles[2]
+
+	if t.find('td', class_='sale_type2').text==unicode('아파트', 'utf-8'):
+		property_type='apt'	
+	else:
+		property_type='house'
+	#print property_type
+	row.append(property_type)
+	dong_name = t.find('td', class_='align_l name')
+	#print dong_name.find('a').text
+	row.append(dong_name.find('a').text.encode('utf-8'))
+	area = re.findall(r"\d+.\d+", t.find('p', class_='calc_area').text)
+	supply_area = area[0]
+	dedicated_area =  area[1]
+	#print supply_area
+	#print dedicated_area
+	row.append(supply_area)
+	row.append(dedicated_area)
+	#print titles[0].find('p', class_='calc_area').text
+	#print titles[0].find('td', class_='num align_r').text
+	price = re.findall(r"\d+,\d+", t.find('td', class_='num align_r').text)
+	#print price[0]
+	row.append(price[0])
+	#for t in titles:
+	#	print t.text
+	return row
 
 f=open('gangnam_gu.csv', 'ab')
 writer = csv.writer(f)
@@ -61,14 +96,20 @@ writer = csv.writer(f)
 header = []
 header.append('Gu')
 header.append('Dong')
+header.append('Property Type')
+header.append('Supply Area')
+header.append('Dedicated Area')
 header.append('Price')
-header.append('Area')
+
 writer.writerow(header)
 
+writer.writerow(test())
 
+'''
 siteUrl = 'http://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1168010300'
 add_page(siteUrl, writer)
 for i in range(1,50):
 	siteUrl = 'http://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01%3AA03%3AA04&cortarNo=1168010300&articleOrderCode=&siteOrderCode=&cpId=&mapX=&mapY=&mapLevel=&minPrc=&maxPrc=&minWrrnt=&maxWrrnt=&minLease=&maxLease=&minSpc=&maxSpc=&subDist=&mviDate=&hsehCnt=&rltrId=&mnex=&mHscpNo=&mPtpRange=&mnexOrder=&location=2932&ptpNo=&bssYm=&page={0}#_content_list_target'.format(i)
 	add_page(siteUrl, writer)
+'''
 f.close()
